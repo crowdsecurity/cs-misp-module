3bc31592da3a2bc5b2596e5463305b382edfdd4e
Julien Loizelet <julienloizelet@okaeli.com>
2024-08-09 17:45:56 +0900

    test(style): Format test files

--- tests/test_crowdsec.py
+++ tests/test_crowdsec.py
@@ -6,7 +6,6 @@ import unittest
 from unittest.mock import MagicMock, patch
 
 from misp_modules.modules.expansion.crowdsec import (
-    _handler_v2,
     get_country_name_from_alpha_2,
     handler,
 )
@@ -21,7 +20,11 @@ def load_file(filename: str):
 
 def remove_uuids(data):
     if isinstance(data, dict):
-        return {key: remove_uuids(value) for key, value in data.items() if key not in ["uuid", "object_uuid"]}
+        return {
+            key: remove_uuids(value)
+            for key, value in data.items()
+            if key not in ["uuid", "object_uuid"]
+        }
     elif isinstance(data, list):
         return [remove_uuids(item) for item in data]
     else:
@@ -34,7 +37,7 @@ class TestCrowdsecModule(unittest.TestCase):
     def setup_class(cls):
         cls.maxDiff = None
 
-    @patch('misp_modules.modules.expansion.crowdsec.requests.get')
+    @patch("misp_modules.modules.expansion.crowdsec.requests.get")
     def test_handler_valid_request(self, mock_get):
         mock_response = MagicMock()
         cti_data = load_file("malicious_ip.json")
@@ -42,10 +45,12 @@ class TestCrowdsecModule(unittest.TestCase):
         mock_response.json.return_value = cti_data
         mock_get.return_value = mock_response
 
-        request_data = json.dumps({
-            "config": {"api_key": "test_api_key"},
-            "attribute": {"type": "ip-src", "value": "1.2.3.4", "uuid": "1234"}
-        })
+        request_data = json.dumps(
+            {
+                "config": {"api_key": "test_api_key"},
+                "attribute": {"type": "ip-src", "value": "1.2.3.4", "uuid": "1234"},
+            }
+        )
         response = handler(request_data)
         self.assertIn("results", response)
         expected_response = load_file("malicious_ip_result.json")
@@ -55,30 +60,41 @@ class TestCrowdsecModule(unittest.TestCase):
 
         self.assertEqual(response_no_uuids, expected_no_uuids)
 
-
-
     def test_handler_missing_config(self):
-        request_data = json.dumps({
-            "attribute": {"type": "ip-src", "value": "1.2.3.4", "uuid": "1234"}
-        })
+        request_data = json.dumps(
+            {"attribute": {"type": "ip-src", "value": "1.2.3.4", "uuid": "1234"}}
+        )
         response = handler(request_data)
         self.assertEqual(response, {"error": "Missing CrowdSec Config"})
 
     def test_handler_missing_api_key(self):
-        request_data = json.dumps({
-            "config": {"key": "value"},
-            "attribute": {"type": "ip-src", "value": "1.2.3.4", "uuid": "1234"}
-        })
+        request_data = json.dumps(
+            {
+                "config": {"key": "value"},
+                "attribute": {"type": "ip-src", "value": "1.2.3.4", "uuid": "1234"},
+            }
+        )
         response = handler(request_data)
         self.assertEqual(response, {"error": "Missing CrowdSec API key"})
 
     def test_handler_invalid_attribute(self):
-        request_data = json.dumps({
-            "config": {"api_key": "test_api_key"},
-            "attribute": {"type": "invalid-type", "value": "1.2.3.4", "uuid": "1234"}
-        })
+        request_data = json.dumps(
+            {
+                "config": {"api_key": "test_api_key"},
+                "attribute": {
+                    "type": "invalid-type",
+                    "value": "1.2.3.4",
+                    "uuid": "1234",
+                },
+            }
+        )
         response = handler(request_data)
-        self.assertEqual(response, {"error": "Wrong input type. Please choose on of the following: ip-dst, ip-src"})
+        self.assertEqual(
+            response,
+            {
+                "error": "Wrong input type. Please choose on of the following: ip-dst, ip-src"
+            },
+        )
 
     def test_get_country_name_from_alpha_2_valid(self):
         country_name = get_country_name_from_alpha_2("US")
@@ -87,4 +103,3 @@ class TestCrowdsecModule(unittest.TestCase):
     def test_get_country_name_from_alpha_2_invalid(self):
         country_name = get_country_name_from_alpha_2("XX")
         self.assertIsNone(country_name)
-
